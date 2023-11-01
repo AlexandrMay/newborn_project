8252e22a92ccd8e6e7683f36115224acb1b78bf9
AlexandrMay <maysalexandr@gmail.com>
2023-11-01 20:01:22 +0200

    sdsd

--- cypress/e2e/first.spec.ts
+++ cypress/e2e/first.spec.ts
@@ -21,9 +21,10 @@ describe("First suite", () => {
   });
 
   it("Third", () => {
-    cy.contains("Асортимент").click();
-    cy.contains("Додати категорію").click();
-    cy.get("input[type='file']").selectFile("cypress/media/photo.jpeg", {force: true});
+    cy.contains("Огляд").click();
+    ApiHelper.getCategories().then((res) => {
+      cy.wrap(res).should('have.length.gte', 2);
+    });
   });
 
   it("Fourth", () => {
